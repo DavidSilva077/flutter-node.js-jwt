ee465f9e0bfa3842522cc30281922b61b662c0c0
DavidSilvaPaiva <david654321123456@gmail.com>
2022-05-19 23:16:38 -0300

    0.0.1

---
+++ android/app/src/main/kotlin/br/com/silva/dashe/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.silva.dashe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:dashe/pages/login_page.dart';
+import 'package:dashe/pages/register_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+      },
+    );
+  }
+}
